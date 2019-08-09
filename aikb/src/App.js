7710910e81ede9c3a6fb54ad39af3092b33fee45
Raynorhs_Thunddye <Raynorhs.Thunddye@gmail.com>
2019-08-09 11:33:45 +0200

    fixed merge conflict

--- aikb/src/App.js
+++ aikb/src/App.js
@@ -2,91 +2,92 @@ import React, { Component, useEffect, useState } from "react";
 import "./App.css";
 import DeviceList from "./Components/PresentationalComponents/DeviceList";
 import System from "./Components/PresentationalComponents/System";
-import { Smartphones } from "./Components/SmartphoneList"
+import Header from "./Components/PresentationalComponents/Header";
+import { Smartphones } from "./Components/SmartphoneList";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            smartphones: null,
-            mockSmartphone: [
-            ],
-            mockSystem: [
-            ]
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      smartphones: null,
+      mockSmartphone: [],
+      mockSystem: []
+    };
+  }
 
-    componentDidMount() {
-        fetch("/get_smartphones").then(response =>
-            response.json().then(data => {
-                this.setState({ smartphones: data.smartphones });
-                this.setState({mockSmartphone: data.smartphones});
-            })
-        );
-    }
+  componentDidMount() {
+    fetch("/get_smartphones").then(response =>
+      response.json().then(data => {
+        this.setState({ smartphones: data.smartphones });
+        this.setState({ mockSmartphone: data.smartphones });
+      })
+    );
+  }
 
-    deleteSmartphoneFromSystem = (index) => {
-        // Remove smartphone from mock system
-        const newMockSystem = Object.assign([], this.state.mockSystem);
-        newMockSystem.splice(index, 1);
-        this.setState({mockSystem: newMockSystem});
+  deleteSmartphoneFromSystem = index => {
+    // Remove smartphone from mock system
+    const newMockSystem = Object.assign([], this.state.mockSystem);
+    newMockSystem.splice(index, 1);
+    this.setState({ mockSystem: newMockSystem });
 
-        this.updateDeviceList(newMockSystem);
-    }
+    this.updateDeviceList(newMockSystem);
+  };
 
-    addSmartphoneToSystem = (smartphone) => {
-        const newMockSystem = Object.assign([], this.state.mockSystem);
-        newMockSystem.push(smartphone);
-        this.updateDeviceList(newMockSystem);
-        
-        this.setState({mockSystem: newMockSystem});
-    }
+  addSmartphoneToSystem = smartphone => {
+    const newMockSystem = Object.assign([], this.state.mockSystem);
+    newMockSystem.push(smartphone);
+    this.updateDeviceList(newMockSystem);
 
-    updateDeviceList = (newMockSystem) => {
-        fetch(`/get_compatible_smartphones_with_system`, {
-            method:'post',
-            headers: {
-                'Accept': 'application/json, text/plain, */*',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({"smartphones": newMockSystem})
-        }).then(response =>
-            response.json().then(data => {
-                var newDatas = newMockSystem.length > 0 ? data.smartphones : this.state.smartphones;
-                this.setState({mockSmartphone: newDatas});
-            })
-        );
-    }
+    this.setState({ mockSystem: newMockSystem });
+  };
 
-    deleteSmartphoneFromDeviceList = (index) => {
-        this.addSmartphoneToSystem(this.state.mockSmartphone[index]);
+  updateDeviceList = newMockSystem => {
+    fetch(`/get_compatible_smartphones_with_system`, {
+      method: "post",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ smartphones: newMockSystem })
+    }).then(response =>
+      response.json().then(data => {
+        var newDatas =
+          newMockSystem.length > 0 ? data.smartphones : this.state.smartphones;
+        this.setState({ mockSmartphone: newDatas });
+      })
+    );
+  };
 
-        const newMockSmartphone = Object.assign([], this.state.mockSmartphone);
-        newMockSmartphone.splice(index, 1);
-        this.setState({mockSmartphone: newMockSmartphone});
-    }
+  deleteSmartphoneFromDeviceList = index => {
+    this.addSmartphoneToSystem(this.state.mockSmartphone[index]);
 
-    render() {
-        const { smartphones } = this.state;
-        return (
-            <div className="App">
-                <div style={{ float: 'left' }}>
-                    {this.state && this.state.smartphones &&
-                        <Smartphones smartphones={smartphones} />
-                    }
-                </div>
-                <div style={{ float: 'right' }}>
-                    <DeviceList 
-                        devices={this.state.mockSmartphone} 
-                        delEvent={this.deleteSmartphoneFromDeviceList}
-                    />
-                    <System devices={this.state.mockSystem}
-                        delEvent={this.deleteSmartphoneFromSystem}
-                    />
-                </div>
-            </div>
-        );
-    }
+    const newMockSmartphone = Object.assign([], this.state.mockSmartphone);
+    newMockSmartphone.splice(index, 1);
+    this.setState({ mockSmartphone: newMockSmartphone });
+  };
+
+  render() {
+    const { smartphones } = this.state;
+    return (
+      <div className="App">
+        <div className="left-page">
+          {this.state &&
+            this.state.smartphones && <Smartphones smartphones={smartphones} />}
+        </div>
+        <Header />
+        <div className="right-page">
+          <DeviceList
+            devices={this.state.mockSmartphone}
+            delEvent={this.deleteSmartphoneFromDeviceList}
+          />
+          <System
+            devices={this.state.mockSystem}
+            delEvent={this.deleteSmartphoneFromSystem}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
