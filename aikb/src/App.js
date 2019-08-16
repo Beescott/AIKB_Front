4ecac44415e12345c53560afbb7d7a4d3797932e
Alexandre <alexandre-nguyen@outlook.fr>
2019-08-16 15:26:06 +0200

    :sparkles: autocomplete search feature

--- aikb/src/App.js
+++ aikb/src/App.js
@@ -3,87 +3,104 @@ import "./App.css";
 import DeviceList from "./Components/PresentationalComponents/DeviceList";
 import System from "./Components/PresentationalComponents/System";
 import Header from "./Components/PresentationalComponents/Header";
+import AutoCompleteSearchBar from "./Components/PresentationalComponents/AutoCompleteSearchBar";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      smartphones: null,
-      mockSmartphone: [],
-      mockSystem: []
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			smartphones: null,
+			mockSmartphone: [],
+			mockSystem: []
+		};
+	}
 
-  componentDidMount() {
-    fetch("/get_smartphones").then(response =>
-      response.json().then(data => {
-        this.setState({ smartphones: data.smartphones });
-        this.setState({ mockSmartphone: data.smartphones });
-      })
-    );
-  }
+	componentDidMount() {
+		fetch("/get_smartphones").then(response =>
+			response.json().then(data => {
+				this.setState({ smartphones: data.smartphones });
+				this.setState({ mockSmartphone: data.smartphones });
+			})
+		);
+	}
 
-  deleteSmartphoneFromSystem = index => {
-    // Remove smartphone from mock system
-    const newMockSystem = Object.assign([], this.state.mockSystem);
-    newMockSystem.splice(index, 1);
-    this.setState({ mockSystem: newMockSystem });
+	deleteSmartphoneFromSystem = index => {
+		// Remove smartphone from mock system
+		const newMockSystem = Object.assign([], this.state.mockSystem);
+		newMockSystem.splice(index, 1);
+		this.setState({ mockSystem: newMockSystem });
 
-    this.updateDeviceList(newMockSystem);
-  };
+		this.updateDeviceList(newMockSystem);
+	};
 
-  addSmartphoneToSystem = smartphone => {
-    const newMockSystem = Object.assign([], this.state.mockSystem);
-    newMockSystem.push(smartphone);
-    this.updateDeviceList(newMockSystem);
+	addSmartphoneToSystem = smartphone => {
+		const newMockSystem = Object.assign([], this.state.mockSystem);
+		newMockSystem.push(smartphone);
+		this.updateDeviceList(newMockSystem);
 
-    this.setState({ mockSystem: newMockSystem });
-  };
+		this.setState({ mockSystem: newMockSystem });
+	};
 
-  updateDeviceList = newMockSystem => {
-    fetch(`/get_compatible_smartphones_with_system`, {
-      method: "post",
-      headers: {
-        Accept: "application/json, text/plain, */*",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ smartphones: newMockSystem })
-    }).then(response =>
-      response.json().then(data => {
-        var newDatas =
-          newMockSystem.length > 0 ? data.smartphones : this.state.smartphones;
-        this.setState({ mockSmartphone: newDatas });
-      })
-    );
-  };
+	updateDeviceList = newMockSystem => {
+		fetch(`/get_compatible_smartphones_with_system`, {
+			method: "post",
+			headers: {
+				Accept: "application/json, text/plain, */*",
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({ smartphones: newMockSystem })
+		}).then(response =>
+			response.json().then(data => {
+				var newDatas =
+					newMockSystem.length > 0 ? data.smartphones : this.state.smartphones;
+				this.setState({ mockSmartphone: newDatas });
+			})
+		);
+	};
 
-  deleteSmartphoneFromDeviceList = index => {
-    this.addSmartphoneToSystem(this.state.mockSmartphone[index]);
+	test = index => {
+		console.log("index : " + index);
+	}
 
-    const newMockSmartphone = Object.assign([], this.state.mockSmartphone);
-    newMockSmartphone.splice(index, 1);
-    this.setState({ mockSmartphone: newMockSmartphone });
-  };
+	deleteSmartphoneFromDeviceList = index => {
+		this.addSmartphoneToSystem(this.state.mockSmartphone[index]);
 
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <div className="left-page">
-          <DeviceList
-            devices={this.state.mockSmartphone}
-            delEvent={this.deleteSmartphoneFromDeviceList}
-          />
-        </div>
-        <div className="right-page">
-          <System
-            devices={this.state.mockSystem}
-            delEvent={this.deleteSmartphoneFromSystem}
-          />
-        </div>
-      </div>
-    );
-  }
+		const newMockSmartphone = Object.assign([], this.state.mockSmartphone);
+		newMockSmartphone.splice(index, 1);
+		this.setState({ mockSmartphone: newMockSmartphone });
+	};
+
+	render() {
+		var smartphoneNameList = [];
+		this.state.mockSmartphone.map((item) =>
+			smartphoneNameList.push(item["name"])
+		);
+
+		return (
+			<div className="App">
+				<Header />
+				<div>
+					<AutoCompleteSearchBar
+						suggestions={smartphoneNameList}
+						deviceList={smartphoneNameList}
+						addEvent={this.deleteSmartphoneFromDeviceList}
+					/>
+				</div>
+				<div className="left-page">
+					<DeviceList
+						devices={this.state.mockSmartphone}
+						delEvent={this.deleteSmartphoneFromDeviceList}
+					/>
+				</div>
+				<div className="right-page">
+					<System
+						devices={this.state.mockSystem}
+						delEvent={this.deleteSmartphoneFromSystem}
+					/>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ aikb/src/Components/PresentationalComponents/AutoCompleteSearchBar/index.js
@@ -0,0 +1,167 @@
+import React, { Component, Fragment } from "react";
+import PropTypes, { func } from "prop-types";
+import './index.css'
+
+class AutoCompleteSearchBar extends Component {
+
+    static defaultProps = {
+        suggestions: []
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            // The active selection's index
+            activeSuggestion: 0,
+            // The suggestions that match the user's input
+            filteredSuggestions: [],
+            // Whether or not the suggestion list is shown
+            showSuggestions: false,
+            // What the user has entered
+            userInput: "",
+            addEvent: props.addEvent
+        };
+    }
+
+    // Event fired when the input value is changed
+    onChange = e => {
+        const { suggestions } = this.props;
+        const userInput = e.currentTarget.value;
+
+        // Filter our suggestions that don't contain the user's input
+        const filteredSuggestions = suggestions.filter(
+            suggestion =>
+                suggestion.toLowerCase().indexOf(userInput.toLowerCase()) > -1
+        );
+
+        // Update the user input and filtered suggestions, reset the active
+        // suggestion and make sure the suggestions are shown
+        this.setState({
+            activeSuggestion: 0,
+            filteredSuggestions,
+            showSuggestions: true,
+            userInput: e.currentTarget.value
+        });
+    };
+
+    // Event fired when the user clicks on a suggestion
+    onClick = e => {
+        const { addEvent } = this.state;
+
+        // Update the user input and reset the rest of the state
+        this.setState({
+            activeSuggestion: 0,
+            filteredSuggestions: [],
+            showSuggestions: false,
+            userInput: e.currentTarget.innerText
+        });
+
+        if (e.currentTarget.innerText != "") {
+            var i = this.props.deviceList.indexOf(e.currentTarget.innerText);
+            addEvent(i);
+        }
+    };
+
+    // Event fired when the user presses a key down
+    onKeyDown = e => {
+        const { activeSuggestion, filteredSuggestions } = this.state;
+
+        // User pressed the enter key, update the input and close the
+        // suggestions
+        if (e.keyCode === 13) {
+            const { activeSuggestion, addEvent } = this.state;
+            
+            if (this.state.filteredSuggestions.length != 0) {
+                var i = this.props.deviceList.indexOf(this.state.filteredSuggestions[activeSuggestion]);
+                addEvent(i);
+            }
+            
+            this.setState({
+                activeSuggestion: 0,
+                showSuggestions: false,
+                userInput: filteredSuggestions[activeSuggestion]
+            });
+        }
+        // User pressed the up arrow, decrement the index
+        else if (e.keyCode === 38) {
+            if (activeSuggestion === 0) {
+                return;
+            }
+
+            this.setState({ activeSuggestion: activeSuggestion - 1 });
+        }
+        // User pressed the down arrow, increment the index
+        else if (e.keyCode === 40) {
+            if (activeSuggestion - 1 === filteredSuggestions.length) {
+                return;
+            }
+
+            this.setState({ activeSuggestion: activeSuggestion + 1 });
+        }
+    };
+
+    render() {
+        const {
+            onChange,
+            onClick,
+            onKeyDown,
+            state: {
+                activeSuggestion,
+                filteredSuggestions,
+                showSuggestions,
+                userInput
+            }
+        } = this;
+
+        let suggestionsListComponent;
+
+        if (showSuggestions && userInput) {
+            if (filteredSuggestions.length) {
+                suggestionsListComponent = (
+                    <ul class="suggestions">
+                        {filteredSuggestions.map((suggestion, index) => {
+                            let className;
+
+                            // Flag the active suggestion with a class
+                            if (index === activeSuggestion) {
+                                className = "suggestion-active";
+                            }
+
+                            return (
+                                <li index={index}
+                                    className={className}
+                                    key={suggestion}
+                                    onClick={onClick}
+                                >
+                                    {suggestion}
+                                </li>
+                            );
+                        })}
+                    </ul>
+                );
+            } else {
+                suggestionsListComponent = (
+                    <div class="no-suggestions">
+                        <em>No suggestions</em>
+                    </div>
+                );
+            }
+        }
+
+        return (
+            <Fragment>
+                <h3>Search a device to add : </h3>
+                <input
+                    type="text"
+                    onChange={onChange}
+                    onKeyDown={onKeyDown}
+                    value={userInput}
+                />
+                {suggestionsListComponent}
+            </Fragment>
+        );
+    }
+}
+
+export default AutoCompleteSearchBar;
